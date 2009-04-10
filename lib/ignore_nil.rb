dcc2936c1eac8e027554a2e2f24ce1cbd7f66c12
Steven Soroka <ssoroka78@gmail.com>
2009-04-10 16:59:15 -0500

    Get rid of extra aliases, I don't think anyone uses them, anyway.

--- lib/ignore_nil.rb
+++ lib/ignore_nil.rb
@@ -1,4 +1,5 @@
 module IgnoreNil
+  # ignore NoMethodError on nil
   def ignore_nil(&block)
     begin
       yield
@@ -11,9 +12,6 @@ module IgnoreNil
       end
     end
   end
-  alias :ignore_nils :ignore_nil
-  alias :quiet_nil :ignore_nil
-  alias :ignore_no_method_on_nil :ignore_nil
 end
 
 Object.send(:include, IgnoreNil)
